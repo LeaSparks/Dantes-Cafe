46530d3a1ddf98f474b374ade6685b9b651062f0
Lea Sparks <lsparksbusinessco@gmail.com>
2026-03-10 18:57:34 -0600

    Enum_Ingredients

---
+++ Assets/_Scripts/Cards/Enum_Ingredients.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public enum IngredientsType
+{
+    Tomato, Bread, Spaghetti, Lemon, Cheese, Bacon, Lettuce, Pepper, Vodka, Potato, Beef, Celery
+}
+
+public class Enum_Ingredients : MonoBehaviour
+{
+ 
+}
